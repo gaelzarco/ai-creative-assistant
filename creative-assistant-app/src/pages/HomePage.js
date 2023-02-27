154ed93c573787b9df5a5cd61c0ac35ab38ef96e
JuanJacoboViera <juanjacoboviera@gmail.com>
2023-02-26 21:22:26 -0500

    intregrate header, main and footer components into HomePage component

--- creative-assistant-app/src/pages/HomePage.js
+++ creative-assistant-app/src/pages/HomePage.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
   )
 }
 
