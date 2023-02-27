ab48697ae84abdc80198991223ad6ef648eafbb1
gael <76548719+gaelzarco@users.noreply.github.com>
2023-02-27 15:01:59 -0800

    .right-container sizing was blocking text-area input box

--- creative-assistant-app/src/components/BriefForm/BriefForm.js
+++ creative-assistant-app/src/components/BriefForm/BriefForm.js
@@ -5,7 +5,7 @@ import './briefForm.css'
 const BriefForm = ({captureVariables, saveData}) => {
   return (
     <form className='form-container' action="">
-      <label htmlFor="briefContext">Write a parragraph describing your campaign</label>
+      <label htmlFor="briefContext">Write a paragraph describing your campaign</label>
       <textarea name="briefContext" id="briefContext" cols="30" rows="10" onChange={(e)=> captureVariables(e) }></textarea>
       <Button saveData={saveData} text='Create Brief Template'/>
     </form>
