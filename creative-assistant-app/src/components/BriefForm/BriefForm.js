717a7576ca6521e5535e87a6dbcd6dd8666b4f25
JuanJacoboViera <juanjacoboviera@gmail.com>
2023-02-27 20:30:02 -0500

    add html and css structure to how it works section in Main component

--- creative-assistant-app/src/components/BriefForm/BriefForm.js
+++ creative-assistant-app/src/components/BriefForm/BriefForm.js
@@ -6,7 +6,7 @@ const BriefForm = ({captureVariables, saveData}) => {
   return (
     <form className='form-container' action="">
       <label htmlFor="briefContext">Write a parragraph describing your campaign</label>
-      <textarea name="briefContext" id="briefContext" cols="30" rows="10" onChange={(e)=> captureVariables(e) }></textarea>
+      <textarea name="briefContext" id="briefContext" cols="30" rows="10" onChange={(e)=> captureVariables(e) } placeholder="The name of our company is [company name] and it specializes in [product industry]."></textarea>
       <Button saveData={saveData} text='Create Brief Template'/>
     </form>
   )
--- creative-assistant-app/src/components/Main/Main.js
+++ creative-assistant-app/src/components/Main/Main.js
@@ -2,6 +2,11 @@ import React, {useState, useEffect} from 'react'
 import './main.css'
 import BriefForm from '../BriefForm/BriefForm'
 import SecondaryTittle from '../SecondaryTittle/SecondaryTittle'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit} from '@fortawesome/free-solid-svg-icons';
+import { faArchive} from '@fortawesome/free-solid-svg-icons';
+import { faDownload} from '@fortawesome/free-solid-svg-icons';
+import { faHandPaper} from '@fortawesome/free-solid-svg-icons';
 
 const Main = () => {
   const [companyName, setCompanyName] = useState('');
@@ -27,8 +32,33 @@ const saveData = () =>{
 
   return (
     <main className='main-container'>
+        <div className="howItWorks-container">
+          <SecondaryTittle text='How it works'/>
+          <div className="instructions-container">
+            <div className="instruction-card">
+            <FontAwesomeIcon icon={faArchive} style={{color: '#ac5eec'}} />
+              <h3>choose sections for template</h3>
+              <p>Choose the neccessary sections that you would like to include in your template</p>
+            </div>
+            <div className="instruction-card">
+              <FontAwesomeIcon icon={faHandPaper} style={{color: '#ac5eec'}} />
+              <h3>Provide information</h3>
+              <p>Provide a summary of your brief, keeping in mind that the more contextual information you provide, the better the output will be.</p>
+            </div>
+            <div className="instruction-card">
+            <FontAwesomeIcon icon={faEdit} style={{color: '#ac5eec'}} />
+              <h3>Edit to your preference</h3>
+              <p>Correct any mistakes or unclear sections that you'd like to correct or rephrase.</p>
+            </div>
+            <div className="instruction-card">
+            <FontAwesomeIcon icon={faDownload} style={{color: '#ac5eec'}} />
+              <h3>Save and download</h3>
+              <p>Save the template in a Mircrosoft Word extension</p>
+            </div>
+          </div>
+        </div>
         <div className="letsGetToTheAction-container">
-        <SecondaryTittle text={'Lets get to the action!'}/>
+        <SecondaryTittle text='Lets get to the action!'/>
         <BriefForm  saveData={saveData} captureVariables={captureVariables}/>
         </div>
     </main>
