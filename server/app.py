1f27592dd28e7c0ac2b3125a3bcf63e6a8bf2e64
gael <76548719+gaelzarco@users.noreply.github.com>
2023-02-28 21:56:32 -0800

    small tweaks to backend

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, request, send_from_directory
 from dotenv import load_dotenv
 import openai
 from docx import Document
